5aea1afd68655abe41c87200bd3b2ddc1deb3805
swapnil280493 <swapnildhage01@gmail.com>
2022-12-10 01:01:24 -0800

    initial commit

---
+++ src/main/java/com/example/SpringBootwithAZServiceBus/SpringBootwithAzServiceBusApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootwithAZServiceBus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootwithAzServiceBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootwithAzServiceBusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootwithAZServiceBus/SpringBootwithAzServiceBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootwithAZServiceBus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootwithAzServiceBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
